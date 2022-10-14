9c7fd4ca417ad0b4d8e560b1dcc58ba64ca44b82
Julyanekamyla <111093710+Julyanekamyla@users.noreply.github.com>
2022-10-14 18:16:56 -0300

    tabuada

--- Projeto Tabuada/script.js
+++ Projeto Tabuada/script.js
@@ -1,21 +1,20 @@
-function carregar() {
-var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('imagem')
-var data = new Date()
-var hora = data.getHours()
-msg.innerHTML = `Agora são ${hora} horas.`
-if(hora > 0 && hora < 12) {
-  img.src='amanhecer.png'
-  document.body.style.background = '#e0c73d'
-  msg2.innerHTML = `Bom Dia`
-}else if(hora >= 12 && hora <= 18){
-  img.src='entardecer.png'
-  document.body.style.background = '#e0a73d'
-  msg2.innerHTML = `Boa Tarde`
-}else {
-  img.src='anoitecer.png'
-  document.body.style.background = '#090908'
-  msg2.innerHTML = `Boa Noite`
-}
+function gerar() {
+  var numero = document.getElementById('numero')
+  var tab = document.getElementById('seltab')
+  if (numero.value.length == 0) {
+    window.alert('Por favor, digite um número!')
+  } else {
+    var n = Number(numero.value)
+    var c = 1 
+    tab.innerHTML = '' //para limpar a tabuada antes de aparecer outra//
+    while(c <= 10) {
+    var item = document.createElement('option') //para criar uma lista dentro do select//
+    item.text = `${n} x ${c} = ${n*c}`
+    item.value = `tab${c}`
+    tab.appendChild(item)
+    c++
+   } 
+
+  }
 
 }
